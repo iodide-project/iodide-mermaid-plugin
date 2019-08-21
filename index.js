54a881dcd1b4e30614b9247d0166841b25db6c2f
William Lachance <wrlach@gmail.com>
2019-08-21 13:08:33 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const MERMAID = 'https://unpkg.com/mermaid@8.0.0/dist/mermaid.min.js'
+
+const loadResource = url => new Promise((resolve) => {
+  const head = document.getElementsByTagName('head')[0];
+  const theScript = document.createElement('script');
+  theScript.src = url;
+  theScript.onload = () => {
+    resolve();
+  };
+  head.appendChild(theScript);
+})
+
+Promise.all([loadResource(MERMAID)])
+  .then(() => {
+    window.mermaidPlugin = {}
+    window.mermaidPlugin.evaluateMermaid = (input) => {
+      const elem = iodide.output.element('div');
+      const id = "iodide-mermaidChart-" + Date.now();
+      elem.setAttribute("class", "iodideMermaid");
+      mermaid.mermaidAPI.render(id, input, (svgCode) => elem.innerHTML = svgCode);
+
+      return elem
+    }
+    window.iodide.addOutputRenderer({
+      shouldRender: val => { return val.getAttribute('class') === 'iodideMermaid'; },
+      render: (val) => {
+        return val.innerHTML;
+      },
+    });
+  });
